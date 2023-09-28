443ef2c54cf3071efe4b81d9a0cde6ee639b38cf
Leonardo Pereira <ethical.leonardo@gmail.com>
2023-09-28 11:39:10 -0300

    permitir cadastrar user

--- teste1.py
+++ teste1.py
@@ -1,1 +1,5 @@
 print('Hello World!')
+name = input("What's is your name? ")
+age = int(input(f'How old are you {name}? '))
+print(f'Name: {name}, Age:{age} years old')
+print("Congratulations! You've done your registration!")
